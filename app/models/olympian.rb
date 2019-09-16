a9dbb6a7bde7abb3d6ab3d8f0713b455b731b40a
Brian Plantico <bplantico@gmail.com>
2019-09-15 22:33:24 -0600

    Adds has_many relationship to Olympian model

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -1,4 +1,9 @@
 class Olympian < ApplicationRecord
+
+  # relationships
+  has_many :olympian_events
+
+  # validations
   validates :name, presence: true, uniqueness: true
   validates :sex, presence: true
   validates :age, presence: true, numericality: { only_integer: true }
--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Olympian, type: :model do
     it { should validate_presence_of :weight }
     it { should validate_presence_of :team }
   end
+
+  describe "relationships" do
+    it { should have_many :olympian_events }
+  end
 end
